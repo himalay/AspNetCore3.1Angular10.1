957eb04a6ca9a8ea8ba942ce7bbcb276c7b59c3c
Himlay <contact@himalay.com.np>
2020-09-06 19:27:40 +0545

    Log SQL query in development mode

--- WorldCities/Startup.cs
+++ WorldCities/Startup.cs
@@ -8,76 +8,94 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WorldCities.Data;
 
-namespace WorldCities {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace WorldCities
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddControllersWithViews ()
-                .AddJsonOptions (options => {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
                     // set this option to TRUE to indent the JSON output
                     options.JsonSerializerOptions.WriteIndented = true;
                     // set this option to NULL to use PascalCase instead of camelCase (default)
                     // options.JsonSerializerOptions.PropertyNamingPolicy = null;
                 });
             // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles (configuration => {
+            services.AddSpaStaticFiles(configuration =>
+            {
                 configuration.RootPath = "ClientApp/dist";
             });
 
-            services.AddHealthChecks ();
+            services.AddHealthChecks();
 
             // Add ApplicationDbContext.
-            services.AddDbContext<ApplicationDbContext> (options =>
-                options.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection"))
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")).EnableSensitiveDataLogging(Env.IsDevelopment())
             );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
-                app.UseExceptionHandler ("/Error");
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts ();
+                app.UseHsts();
             }
 
-            app.UseHttpsRedirection ();
-            app.UseStaticFiles (new StaticFileOptions () {
-                OnPrepareResponse = (context) => {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = (context) =>
+                {
                     // Retrieve cache configuration from appsettings.json
                     context.Context.Response.Headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
                     context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
                     context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
                 }
             });
-            if (!env.IsDevelopment ()) {
-                app.UseSpaStaticFiles ();
+            if (!Env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
             }
 
-            app.UseHealthChecks ("/health");
-            app.UseRouting ();
+            app.UseHealthChecks("/health");
+            app.UseRouting();
 
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllerRoute (
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            app.UseSpa (spa => {
+            app.UseSpa(spa =>
+            {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment ()) {
-                    spa.UseAngularCliServer (npmScript: "start");
+                if (Env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
                 }
             });
         }
